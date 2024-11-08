feae26a2855906e15c8855fdf3b96a90f39404a4
Josh Pablico <93159092+jpablico@users.noreply.github.com>
2024-11-08 14:00:14 -0600

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"], // Files to lint
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node }, // Support both browser and Node.js environments
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "error", // Prettier errors are shown by ESLint
+    },
+  },
+  pluginJs.configs.recommended, // Base ESLint rules
+  pluginReact.configs.flat.recommended, // React-specific rules
+];
