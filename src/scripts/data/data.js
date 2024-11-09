1bb05141aff6a6f752f7f1558d695db384b57a9c
Josh Pablico <jbpablico27@gmail.com>
2024-11-08 22:55:05 -0600

    Testing weather api.

--- src/scripts/data/data.js
+++ src/scripts/data/data.js
@@ -1,7 +1,21 @@
-fetch(
-  'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Mckinney,Tx/?key=CK8PXYBKEK687PAUVQAXREQWH'
-)
-  .then((response) => response.json())
-  .then((data) => {
+async function getWeather(location = 'Mckinney,TX') {
+  const apiKey = 'CK8PXYBKEK687PAUVQAXREQWH';
+  const baseUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline`;
+
+  try {
+    const response = await fetch(`${baseUrl}/${location}/?key=${apiKey}`);
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    
+    const data = await response.json();
     console.log(data);
-  });
+    return data;
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    return null;
+  }
+}
+
+export { getWeather };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,22 @@
 import React, { useEffect } from 'react';
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
+import { getWeather } from './data/data';
 
 const App = () => {
   useEffect(() => {
     console.log('Component mounted');
+    getWeather();
+
+    (async function () {
+      console.log('Ambiguous city  ---');
+      await getWeather('Springfield'); // Test an ambiguous city name
+      console.log('Specific city ---');
+      await getWeather('Springfield, MO'); // Test specific city/state
+      console.log('Specific city ---');
+      await getWeather('Everett'); // Test specific
+    })();
+    
   }, []);
 
   return <h1>Hello, World!</h1>;
