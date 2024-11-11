5d451020d80017aa932a12cfc87637178b291b44
Josh Pablico <jbpablico27@gmail.com>
2024-11-11 15:36:47 -0600

    Devlog

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -4,6 +4,15 @@ import "../../styles/style.scss";
 function Header() {
   return (
     <header id = 'header'>
+      <div className = 'user-container'>
+        <div className = 'user-icon'>
+          <span className="material-symbols-outlined user-icon">account_circle</span>
+        </div>
+        <div className = 'user-info'>
+          <h5 className='user-name'>Hey, Josh</h5>
+          <h2 className='date'>Monday, November 11th</h2>
+        </div>
+      </div>
       <div className = 'search-container'>
         <input type = 'text' id = 'search-bar' placeholder = 'Search for a location'></input>
         <span className="material-symbols-outlined">search</span>
